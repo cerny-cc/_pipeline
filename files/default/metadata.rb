f860ab9ac067ce4c813dfaeccb1265ab4a8ca4fc
Nathan Cerny <ncerny@gmail.com>
2017-04-11 10:30:50 -0500

    Initial Commit.
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

---
+++ files/default/metadata.rb
@@ -0,0 +1,8 @@
+name 'build_cookbook'
+maintainer 'Nathan Cerny'
+maintainer_email 'ncerny@gmail.com'
+license 'apachev2'
+version '0.1.0'
+
+depends 'delivery-truck'
+depends '_build'
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name '_external'
+maintainer 'Nathan Cerny'
+maintainer_email 'ncerny@gmail.com'
+license 'apachev2'
+version '1.0.0'
+
+depends 'delivery-sugar'
+depends 'tar'
